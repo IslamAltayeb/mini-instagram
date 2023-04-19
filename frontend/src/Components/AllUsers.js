44f3cfc3a11f3fe4821338f10263675050718946
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-04-19 12:43:19 +0200

    add all users to the homepage

---
+++ frontend/src/Components/AllUsers.js
@@ -0,0 +1,41 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import React from "react";
+import "../Style/UserArea.css"
+import {Link} from "react-router-dom";
+
+export default function AllUsers(){
+    const [users, setUsers] = useState([]);
+
+    useEffect(() =>{
+        axios.get(`/allUsers`)
+            .then(result =>{
+                setUsers(result.data.user)
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    }, [])
+
+
+    return(
+        <div className="flex-row users-area">
+            {
+                users && users.map(user=>{
+                    return(
+                        <div key={user._id}>
+                            <Link to={`/user/${user._id}`}>
+                                {
+                                    user.image ?
+                                        <span className="profile-img-users">
+                                    <img src={"http://localhost:2100"+user.image} alt="avatar"/>
+                                </span> : null
+                                }
+                            </Link>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
